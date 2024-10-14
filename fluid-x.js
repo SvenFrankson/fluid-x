c91b5c4854eaaba08ba73590315f12061773f896
SvenFrankson <sven.frankson@gmail.com>
2024-10-14 03:01:19 +0200

    Add LevelPage pagination.

--- fluid-x.js
+++ fluid-x.js
@@ -793,6 +793,8 @@ class HoleTile extends Tile {
 }
 class LevelPage {
     constructor(queryString) {
+        this.page = 0;
+        this.levelCount = 100;
         this.nabuPage = document.querySelector(queryString);
     }
     get shown() {
@@ -810,19 +812,61 @@ class LevelPage {
         let rowCount = Math.floor(rect.height * 0.7 / 90);
         let container = this.nabuPage.querySelector(".square-btn-container");
         container.innerHTML = "";
-        let n = 0;
-        for (let i = 0; i < rowCount; i++) {
+        let levelsPerPage = colCount * (rowCount - 1);
+        let maxPage = Math.ceil(this.levelCount / levelsPerPage);
+        let n = this.page * levelsPerPage;
+        for (let i = 0; i < rowCount - 1; i++) {
             let line = document.createElement("div");
             line.classList.add("square-btn-container-line");
             container.appendChild(line);
             for (let j = 0; j < colCount; j++) {
                 let squareButton = document.createElement("button");
                 squareButton.classList.add("square-btn");
+                if (n >= this.levelCount) {
+                    squareButton.style.visibility = "hidden";
+                }
+                else {
+                    squareButton.innerHTML = "<stroke-text>" + (n + 1).toFixed(0) + "</stroke-text>";
+                }
                 n++;
-                squareButton.innerHTML = "<stroke-text>Level " + n.toFixed(0) + "</stroke-text>";
                 line.appendChild(squareButton);
             }
         }
+        let line = document.createElement("div");
+        line.classList.add("square-btn-container-line");
+        container.appendChild(line);
+        let prevButton = document.createElement("button");
+        prevButton.classList.add("square-btn");
+        if (this.page === 0) {
+            prevButton.innerHTML = "<stroke-text>BACK</stroke-text>";
+        }
+        else {
+            prevButton.innerHTML = "<stroke-text>PREV</stroke-text>";
+            prevButton.onclick = () => {
+                this.page--;
+                this.redraw();
+            };
+        }
+        line.appendChild(prevButton);
+        for (let j = 1; j < colCount - 1; j++) {
+            let squareButton = document.createElement("button");
+            squareButton.classList.add("square-btn");
+            squareButton.style.visibility = "hidden";
+            line.appendChild(squareButton);
+        }
+        let nextButton = document.createElement("button");
+        nextButton.classList.add("square-btn");
+        if (this.page < maxPage - 1) {
+            nextButton.innerHTML = "<stroke-text>NEXT</stroke-text>";
+            nextButton.onclick = () => {
+                this.page++;
+                this.redraw();
+            };
+        }
+        else {
+            nextButton.style.visibility = "hidden";
+        }
+        line.appendChild(nextButton);
     }
 }
 /// <reference path="../lib/nabu/nabu.d.ts"/>
